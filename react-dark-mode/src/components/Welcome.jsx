f58285789daff54b2c454cad6b50cb805d854a8a
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-01-05 19:40:21 -0500

    Remove 'learn more' button

--- react-dark-mode/src/components/Welcome.jsx
+++ react-dark-mode/src/components/Welcome.jsx
@@ -4,7 +4,6 @@ const Welcome = (props) => {
     <section>
       <h2>Welcome to Our Software Development Company</h2>
       <p>We specialize in building custom software solutions for businesses of all sizes. Our team of experienced developers is ready to bring your project to life.</p>
-      <button>Learn More</button>
     </section>
     <section>
       <h2>What Our Clients Say</h2>
